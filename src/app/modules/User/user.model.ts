d33e801fc43b1e5e634a8120680446b5f5136170
DeveloperDolon <dolonr718@gmail.com>
2025-05-02 18:45:21 +0600

    user model added

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,22 +1,62 @@
-import { Model } from "mongoose";
+import { model, Schema } from 'mongoose';
 
-export interface TUser {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-}
+import bcrypt from 'bcrypt';
+import config from '../../config';
+import { v4 as uuidv4 } from 'uuid';
+import { TUser, UserModel } from './user.interface';
 
-export type TLoginUser = {
-  email: string;
-  password: string;
+const userSchema = new Schema<TUser, UserModel>(
+  {
+    id: {
+      type: String,
+      unique: true,
+      default: () => uuidv4(),
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (this: TUser, next) {
+  const user = this;
+
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_solid_rounds),
+  );
+
+  next();
+});
+
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+userSchema.statics.isUserExistByEmail = async function (email: string) {
+  return await this.findOne({ email }).select('+password');
 };
 
-export interface UserModel extends Model<TUser> {
-    isUserExistByEmail(email: string): Promise<TUser | null>;
-    isPasswordMatched(
-      plainTextPassword: string,
-      hashedPassword: string,
-    ): Promise<boolean>;
-  }
-  
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword,
+  hashedPassword,
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+export const User = model<TUser, UserModel>('User', userSchema);
