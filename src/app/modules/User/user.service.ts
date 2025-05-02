cde2b3d8f52b45b337b36972c390176daa803cec
DeveloperDolon <dolonr718@gmail.com>
2025-05-02 18:43:13 +0600

    user interface added

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -1,5 +1,7 @@
 import { Request } from 'express';
 
-const createUserIntoDB = async (req: Request) => {};
+const createUserIntoDB = async (req: Request) => {
+    
+};
 
 export const UserService = { createUserIntoDB };
