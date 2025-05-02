8f8ae9616c24708d5d2563291d72e6f10adb9fad
DeveloperDolon <dolonr718@gmail.com>
2025-05-02 20:40:39 +0600

    user password validation enhance

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 const userCreateValidation = z.object({
   body: z.object({
-    password: z.string().min(8, 'Password must be at least 8 character!'),
+    password: z.string().min(5, 'Password must be at least 5 character!'),
     user: z.object({
       name: z.string().min(3, 'Name must be at least 3 character!'),
       email: z.string().email(),
