f088214660bca81b1368fb787497358a940eef2c
DeveloperDolon <dolonr718@gmail.com>
2025-05-02 18:51:22 +0600

    user validation schemas added

--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -1,16 +1,22 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-export const userCreateValidation = z.object({
-    body: z.object({
-        password: z.string().min(8, "Password must be at least 8 character!"),
-        user: z.object({
-            name: z.string().min(3, "Name must be at least 3 character!"),
-            email: z.string().email()
-        })
-    })
-})
+const userCreateValidation = z.object({
+  body: z.object({
+    password: z.string().min(8, 'Password must be at least 8 character!'),
+    user: z.object({
+      name: z.string().min(3, 'Name must be at least 3 character!'),
+      email: z.string().email(),
+    }),
+  }),
+});
 
+const userLoginValidation = z.object({
+  body: z.object({
+    email: z.string().email(),
+    password: z.string().max(20),
+  }),
+});
 
-const UserValidation = { userCreateValidation };
+const UserValidation = { userCreateValidation, userLoginValidation };
 
 export default UserValidation;
