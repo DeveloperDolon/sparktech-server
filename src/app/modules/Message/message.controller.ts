80b72a26724021dbb982f35b5a4a2f975df8d077
DeveloperDolon <dolonr718@gmail.com>
2025-09-12 12:16:33 +0600

    some changes

--- src/app/modules/Message/message.controller.ts
+++ src/app/modules/Message/message.controller.ts
@@ -1,6 +1,8 @@
+import { ChatRoom } from './../ChatRoom/chatroom.model';
 import type { Socket } from 'socket.io';
 import { Message } from './message.model';
 import { getReceiverSocketId } from '../../lib/socket';
+import { get } from 'node:https';
 
 const messageFunction = (socket: Socket) => {
   socket.on(
@@ -11,11 +13,18 @@ const messageFunction = (socket: Socket) => {
       authId: string;
       roomId: string;
     }) => {
+
       const newMessage = await Message.create({
         chatRoom: data?.roomId,
         content: data?.message,
         sender: data?.authId,
         receiverId: data?.userId,
+      });
+      const chatRoom = await ChatRoom.findOne({id: data?.roomId});
+
+      socket.to(getReceiverSocketId(data.userId)).emit('chatroom', {
+        chatRoom,
+        newMessage: data?.message,
       });
 
       socket.to(getReceiverSocketId(data.userId)).emit('message', {
